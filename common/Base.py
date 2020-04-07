e557361a2e2e122fee8905c79cdee9995e41b777
denghui <314983713@qq.com>
2020-04-07 14:15:12 +0800

    base方法更新

--- common/Base.py
+++ common/Base.py
@@ -9,7 +9,11 @@ from utils.AssertUitl import AssertUitl
 p_data = '\${(.*)}\$'
 log = my_log()
 
-def init_db(db_alias='db_1'):       # 默认db_1  数据库
+def init_db(db_alias='db_1'):
+    """
+    :param db_alias: # 默认db_1  数据库
+    :return: sql的光标对象
+    """
     db_info = ConfigYaml().get_db_conf_info(db_alias)
     host  = db_info["db_host"]
     user  = db_info["db_user"]
@@ -22,9 +26,10 @@ def init_db(db_alias='db_1'):       # 默认db_1  数据库
 
 def json_parse(data):
     """
-    格式化字符  转换json格式
-    :param data:
-    :return:
+    转换json格式
+    :param data:需要格式转译格式化字符
+    :return: 转译后的数据
+    不晓得有啥用
     """
       # 1.判断headers是否存在，json转义，无需
         # if headers:
@@ -39,28 +44,13 @@ def json_parse(data):
         #     cookie = cookies
     return json.loads(data) if data else data
 
-
-
-def json_parse(data):
-    """
-    格式化字符，转换json
-    :param data:
-    :return:
-    """
-    # if headers:
-    #     header = json.loads(headers)
-    # else:
-    #     header = headers
-    return json.loads(data) if data else data
-
 def res_find(data,pattern_data=p_data):
     """
     查询
-    :param data:
-    :param pattern_data:
+    :param data:需要提取的原数据
+    :param pattern_data: 正则表达提取格式
     :return:
     """
-    #pattern = re.compile('\${(.*)}\$')
     pattern = re.compile(pattern_data)
     re_res = pattern.findall(data)
     return re_res
@@ -68,10 +58,10 @@ def res_find(data,pattern_data=p_data):
 def res_sub(data,replace,pattern_data=p_data):
     """
     替换
-    :param data:
-    :param replace:
-    :param pattern_data:
-    :return:
+    :param data:原内容
+    :param replace: 替换内容
+    :param pattern_data:正则表达提取格式
+    :return: 替换后实际内容
     """
     pattern = re.compile(pattern_data)
     re_res = pattern.findall(data)
@@ -82,6 +72,7 @@ def res_sub(data,replace,pattern_data=p_data):
 def params_find(headers,cookies):
     """
     验证请求中是否有${}$需要结果关联
+    不晓得怎么用
     :param headers:
     :param cookies:
     :return:
@@ -92,45 +83,45 @@ def params_find(headers,cookies):
         cookies = res_find(cookies)
     return headers,cookies
 
-# def allure_report(report_path,report_html):
-#     """
-#     生成allure 报告
-#     :param report_path:
-#     :param report_html:
-#     :return:
-#     """
-#     #执行命令 allure generate
-#     allure_cmd ="allure generate %s -o %s --clean"%(report_path,report_html)
-#     #subprocess.call
-#     log.info("报告地址")
-#     try:
-#         subprocess.call(allure_cmd,shell=True)
-#     except:
-#         log.error("执行用例失败，请检查一下测试环境相关配置")
-#         raise
+def allure_report(report_path,report_html):
+    """
+    生成allure 报告
+    :param report_path:
+    :param report_html:
+    :return:
+    """
+    #执行命令 allure generate
+    allure_cmd ="allure generate %s -o %s --clean"%(report_path,report_html)
+    #subprocess.call
+    log.info("报告地址")
+    try:
+        subprocess.call(allure_cmd,shell=True)
+    except:
+        log.error("执行用例失败，请检查一下测试环境相关配置")
+        raise
 
-# def send_mail(report_html_path="",content="",title="测试"):
-#     """
-#     发送邮件
-#     :param report_html_path:
-#     :param content:
-#     :param title:
-#     :return:
-#     """
-#     email_info = ConfigYaml().get_email_info()
-#     smtp_addr = email_info["smtpserver"]
-#     username = email_info["username"]
-#     password = email_info["password"]
-#     recv = email_info["receiver"]
-#     email = SendEmail(
-#         smtp_addr=smtp_addr,
-#         username=username,
-#         password=password,
-#         recv=recv,
-#         title=title,
-#         content=content,
-#         file=report_html_path)
-#     email.send_mail()
+def send_mail(report_html_path="",content="",title="测试"):
+    """
+    发送邮件
+    :param report_html_path:
+    :param content:
+    :param title:
+    :return:
+    """
+    email_info = ConfigYaml().get_email_info()
+    smtp_addr = email_info["smtpserver"]
+    username = email_info["username"]
+    password = email_info["password"]
+    recv = email_info["receiver"]
+    email = SendEmail(
+        smtp_addr=smtp_addr,
+        username=username,
+        password=password,
+        recv=recv,
+        title=title,
+        content=content,
+        file=report_html_path)
+    email.send_mail()
 
 def assert_db(db_name,result,db_verify):
     """
@@ -159,7 +150,8 @@ def assert_db(db_name,result,db_verify):
         assert_util.assert_body(res_line, res_db_line)
 
 if __name__ == '__main__':
-    #print(init_db("db_1"))
+    pass
+    print(init_db("db_1"))
     print(res_find( '{"Authorization": "JWT ${token}$"}'))
     print(res_sub( '{"Authorization": "JWT ${token}$"}',"123"))
     #print(params_find())
